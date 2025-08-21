19896b4aa9c28bfda1b132798cf47164ed73dd95
arpitsha26 <arpitsharma1263@gmail.com>
2025-08-21 23:45:05 +0530

    BACKEND: Edit profile & Get profile Controllers

--- backend/config/Mail.js
+++ backend/config/Mail.js
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
 const sendMail = async (to, otp) => {
   try {
     await transporter.sendMail({
-      from: `"Crewzy-" <${process.env.EMAIL}>`,
+      from: `"Crewzy" <${process.env.EMAIL}>`,
       to,
       subject: "Reset Your Password",
       html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`,
